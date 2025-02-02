b87fc2878ba56e2b3455d2849f6a21f9df3a4675
brunocoronado49 <francorangelcoronado@gmail.com>
2025-02-02 13:56:57 -0600

    Initial commit

---
+++ lib/presentation/screens/header_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:custom_painter/presentation/widgets/widgets.dart';
+
+class HeaderScreen extends StatelessWidget {
+  const HeaderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HeaderCuadrado(),
+    );
+  }
+}
+
---
+++ lib/presentation/widgets/headers.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HeaderCuadrado extends StatelessWidget {
+  const HeaderCuadrado({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: const Color.fromARGB(255, 54, 212, 244),
+    );
+  }
+}
+

1868d309f03952b99fc06bbf22a64eb1b47c9e8f
brunocoronado49 <francorangelcoronado@gmail.com>
2025-02-04 00:15:26 -0600

    💄 Finish section one

--- lib/presentation/widgets/headers.dart
+++ lib/presentation/widgets/headers.dart
@@ -181,7 +181,48 @@ class _HeaderCurvoPainter extends CustomPainter {
     final path = Path();
 
     path.lineTo(0, size.height * 0.25);
-    path.quadraticBezierTo(size.width * 0.5, size.height * 0.40, size.width, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.5, size.height * 0.55, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+
+}
+
+class HeaderWaves extends StatelessWidget {
+  const HeaderWaves({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderWavesPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderWavesPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = Paint();
+
+    lapiz.color = Color.fromARGB(255, 54, 212, 244);
+    lapiz.style = PaintingStyle.fill;
+    lapiz.strokeWidth = 15;
+
+    final path = Path();
+
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.30, size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.20, size.width, size.height * 0.25);
     path.lineTo(size.width, 0);
 
     canvas.drawPath(path, lapiz);

62a0843481f04f7605a6b4c3d022ee400205b4cc
brunocoronado49 <francorangelcoronado@gmail.com>
2025-02-03 19:02:19 -0600

    💄 Update header using custom painter (custom painter diagonal and triangulo)

--- lib/presentation/widgets/headers.dart
+++ lib/presentation/widgets/headers.dart
@@ -35,10 +35,84 @@ class HeaderDiagonal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      color: const Color.fromARGB(255, 54, 212, 244),
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
     );
   }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = Paint();
+
+    // props to lapiz
+    lapiz.color = Color.fromARGB(255, 54, 212, 244);
+    lapiz.style = PaintingStyle.fill;
+    lapiz.strokeWidth = 5;
+
+    final path = Path();
+
+    // dibujar con el path y lapiz
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+
+}
+
+class HeaderTriangulo extends StatelessWidget {
+  const HeaderTriangulo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderTrianguloPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderTrianguloPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = Paint();
+
+    // props to lapiz
+    lapiz.color = Color.fromARGB(255, 54, 212, 244);
+    lapiz.style = PaintingStyle.fill;
+    lapiz.strokeWidth = 5;
+
+    final path = Path();
+
+    // dibujar con el path y lapiz
+    path.lineTo(size.width, size.height);
+    // path.lineTo(size.width, 0);
+    path.lineTo(0, size.height);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+
 }
 

06692d1d1aa2c447e0edea7d3f1b281b5c8f2bc2
brunocoronado49 <francorangelcoronado@gmail.com>
2025-03-23 12:15:15 -0600

    Update animation in animation screen

--- lib/presentation/screens/animations_screen.dart
+++ lib/presentation/screens/animations_screen.dart
@@ -20,7 +20,28 @@ class CuadradoAnimado extends StatefulWidget {
   State<CuadradoAnimado> createState() => _CuadradoAnimadoState();
 }
 
-class _CuadradoAnimadoState extends State<CuadradoAnimado> {
+class _CuadradoAnimadoState extends State<CuadradoAnimado> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> rotation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 4000)
+    );
+
+    rotation = Tween(begin: 0.0, end: 2.0).animate(controller);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return _Rectangulo();
